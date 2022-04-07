d58226550268bba81b0c66f625f8970e6f163bde
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-04-07 12:44:04 +0700

    add design form edit user

--- src/rifqimuhammadaziz/view/user/EditForm.java
+++ src/rifqimuhammadaziz/view/user/EditForm.java
@@ -5,4 +5,13 @@ import java.awt.*;
 
 public class EditForm extends Container {
     JPanel rootPanel;
+    private JFormattedTextField formattedTextField1;
+    private JTextField textField1;
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+    private JTextArea textArea1;
+    private JTextField textField2;
+    private JButton updateButton;
+    private JButton deleteButton;
+    private JButton disableButton;
 }
